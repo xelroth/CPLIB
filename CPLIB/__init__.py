ffe11c93d3e627a42e8621b9344014c415a722ae
zelrorh <142230054+xelroth@users.noreply.github.com>
2024-03-11 18:07:42 +0330

    __init__.py

--- CPLIB/__init__.py
+++ CPLIB/__init__.py
@@ -1,6 +1,1 @@
-from . import Cpanel
-from .Cpanel import FileManager
-
-class Main(Cpanel.Cpanel, FileManager.FileManager):
-    def __init__(self):
-        super().__init__()
+from .Cpanel import Cpanel,FileManager
